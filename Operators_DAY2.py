f34a3886e028796cd24d771a58a8376b00774028
meghanavaddi2402 <meghanavaddi2402@gmail.com>
2026-07-02 18:48:15 +0530

    Add files via upload
    This demonstrates the different types of operators in Python through practical examples. The concepts covered include Arithmetic, Assignment, Comparison ,Logical, Identity, Membership, and Bitwise operators, helping build a strong foundation in performing calculations, comparisons, logical operations, object identity checks, membership testing, and binary operations in Python.

---
+++ Operators_DAY2.py
@@ -0,0 +1,82 @@
+'''
+OPERATORS -
+1.Arithematic Operator
++, -, /, **, //, %
+Example :-'''
+a = 10
+b = 10
+print(a + b)
+'''
+2.Assignment Operator
+=, +=(INCREMENT), -=(DECREMENT), %=
+Example :-'''
+a = 10
+a +=  6
+print(a)
+'''
+3.Comparision Operator
+==, !=, <=, >=, < , >
+Example :-'''
+a = 10
+b = 5
+print(a <= b)
+'''
+4.Logical Operator
+AND => Returns true if both are true
+Example :-'''
+a = 7
+b = 6
+c = 5
+print ( a>=b and b>=c)
+'''
+OR => Returns true if anyone is true
+Example :-'''
+a = 7
+b = 6
+c = 5
+print ( a>=b or b>=c)
+'''
+NOT => Returns true if false and false if true
+Example :-'''
+a = 7
+b = 6
+c = 5
+print (not( a>=b or b>=c))
+'''
+
+'''
+5.Identity Operator
+is => Same object or not
+Example :-'''
+a = 24
+b = 24
+print(a == b)
+print( a is b)
+
+'''
+is not => Not same object
+Example :-'''
+a = 24
+b = 24
+print(a == b)
+print( a is not b)
+
+'''6.Membership Operator
+in => Checks if it is there
+Example :-'''
+a = [1,2,3,4,5,6]
+print( 3 in a)
+'''
+not in => Checks if not there
+Example :-'''
+a = [1,2,3,4,5,6]
+print( 24 not in a)
+'''
+'''
+7.Bitwise Operator
+&, <<, >>,  |, ^
+Example :-'''
+print(2 & 7)
+print(2 | 7)
+
+
