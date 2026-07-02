2f748269d6f68bce49a55811fd0db1b7d501e320
meghanavaddi2402 <meghanavaddi2402@gmail.com>
2026-07-02 18:39:41 +0530

    Add files via upload
    This file demonstrates the basic concepts of Python programming learned on.Day 1 of my 100 Days Python Full Stack Challenge. It includes an introduction to Python, Procedural vs Object-Oriented Programming, Python features, variables, tokens, naming conventions, comments, Boolean values, and conditional statements with practical examples.

---
+++ Introduction_DAY1.py
@@ -0,0 +1,75 @@
+'''
+=> Procedural Language
+
+	This follows step-by-step approach, where code is structured into procedure such as functions or routines
+	EXAMPLE :- C Programming
+
+=> Object Oriented Programming Language
+
+	This is based on concepts of objects and classes
+	Example :- Python and Java
+
+
+=> Python
+1.Dynamically typed :- Python know the data we are passing to the variable
+2.Interpreter :- Python execute code line-by-line, if any error it will stop the execution in that line and gives the output of the previous lines
+Example :-'''
+print(89+67)
+num = 89
+num_2 = 67
+total_ = num + num_2
+print(id(num))
+print(id(num_2))
+print(id(total))
+print(total_)
+'''
+3.High_level :-
+Example :-
+int num = 89
+=> Python :- 1991,Guide Van Rossem
+
+=> why use Python :-
+	-Easy syntax
+	-Cross-platform
+	-Wide applications
+	-Huge library support
+
+=> Tokens
+	1.Keywords :- These are reserved word in python
+	Example :- if, else, for, while
+	2.Identifiers :- Names given to the variables, functions, classes
+	3.Literals
+	Example :- 89, "Hello", 2.94
+	4.Operators :- +, -, *, /
+	5.Punctuations :- (), {}, []
+
+=> Rules of variables
+	Good Ways :-
+	 A-Z, a-z,_
+	 Example :- NUM = 7
+		    num = 7
+		    num_1 = 7
+	Bad Ways :-
+	 Special characters, number, Space
+	 Example :- @NUM = 24
+		    nu#m = 24
+		    nu m = 24
+		    2num = 24
+=> Comments :-
+	-single line :- #
+	-multi-line :- ''' ''', """ """
+	Example :-'''
+num = 24
+if num % 2 == 0:
+    print("Even")
+else:
+    print("odd")
+'''
+=> Boolean :-
+	1.True
+	2.False
+	Example :-
+		   num = 2
+		   num_1 = 2
+		   print(num == num_1)
+
