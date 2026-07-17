0200c7c406db18bae857e7c309aeaad3611ed6f7
meghanavaddi2402 <meghanavaddi2402@gmail.com>
2026-07-17 22:34:22 +0530

    Add files via upload
    This file demonstrates how to send emails programmatically using Python's built-in `smtplib`, `ssl`, and `email` modules. The program establishes a secure SMTP connection, authenticates the sender, composes an email with a subject and message body, and sends it to the recipient.
    
    Topics Covered:
    • Introduction to SMTP (Simple Mail Transfer Protocol)
    • Sending Emails Using Python
    • EmailMessage Class
    • Secure SMTP Connection with SSL/TLS
    • SMTP Authentication
    • Gmail SMTP Server Configuration
    • Creating and Sending Email Messages
    • Using the smtplib, ssl, and email Modules

---
+++ DAY_18.py
@@ -0,0 +1,27 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+sender_email = "meghanavaddi2402@gmail.com"
+password = "awaaevxocgrnhizb"
+receiver_mail = "vaddi1234YT@gmail.com"
+message = EmailMessage()
+message ["From"] =   "meghanavaddi2402@gmail.com"
+message ["To"] =   "vaddi1234YT@gmail.com"
+message["Subject"] = "Welcome Mail"
+message.set_content(f"""
+Hello Meghana!!
+
+Welcome to our python class
+
+Regards,
+Python Team
+""")
+
+context = ssl.create_default_context()
+with smtplib.SMTP("smtp.gmail.com", port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls(context=context)
+    smtp.ehlo()
+    smtp.login(sender_email,password)
+    smtp.send_message(message)
+                                                                                                                                                                                        print("Functions_DAY12.py
