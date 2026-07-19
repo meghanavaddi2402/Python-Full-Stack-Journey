5e122c3a65b2085b0b73d4c01fa26006aa5663ca
meghanavaddi2402 <meghanavaddi2402@gmail.com>
2026-07-19 16:56:47 +0530

    Update OOP_concepts_DAY_20.py

--- OOP_concepts_DAY_20.py
+++ OOP_concepts_DAY_20.py
@@ -1,20 +1,45 @@
 '''
-class how:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def nam(self):
-        print(self.name)
-s1 = how("Maggi",34)
-print(s1.nam())
 
-=> Methods :- Methods are nothing but the functions inside the class
+                                        OOPS
+                                        ----
+
+--> Object-oriented programming System(oops), This will be organizes the code using classes and objects....
+
+USES:
+-Code reusable
+-Easy maintaince
+-Clear understanding
+-Better security
+
+Classes:
+- It is a blue print or a template used to create an object..
+
+class class_name:
+    pass
+    
+Objects:
+- Object is a instance of the class
+
+class student:
+    std_name = 'Meenakshi' #Class attribute
+
+st_ = student()
+print(st_)
+
+
+Attributes:
+- Attributes are the variables that belongs to an object or the class
+    
 '''
-class calc:
-    def add(self, num1, num2):
-        print(num1 + num2)
-    def sub(self, num1, num2):
-        print(num1 - num2)
-c = calc()
-c.add(2,90)
-c.sub(8, 9)
+
+    
+class how:
+    def details(self, name, age):
+        self.name = name  #instance attribute
+        self.age = age
+    def name(self):
+        print(self.name)
+s1_ = how()
+s1_.details('Meenakshi',21)
+
+print(f"Name: {s1_.name}\nAge: {s1_.age}")
